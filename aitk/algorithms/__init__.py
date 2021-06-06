d34a7b9ecaf3694050baf3f89e8588ed3ccd8e20
Doug Blank <doug.blank@gmail.com>
2021-06-06 10:24:08 -0700

    Expose __version__; Version 0.0.4

---
+++ aitk/algorithms/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# ****************************************************************
+# aitk.algorithms: Python algorithms for AI
+#
+# Copyright (c) 2021 AITK Developers
+#
+# https://github.com/ArtificialIntelligenceToolkit/aitk.algorithms
+#
+# ****************************************************************
+
+from ._version import __version__  # noqa: F401
